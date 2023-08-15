2e0d1802d3bc9ed6017b5b2be4679d365be720d8
vasu762 <vasuvaghasiya10@gmail.com>
2023-08-15 06:57:37 -0700

    ds

--- opretor.c
+++ opretor.c
@@ -1,5 +1,21 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-     int a=1, b=2, c=1, d; 
+    int a = 1, b = 2, c = 1, d;
+    //  d = a || --b || c;
+    // printf("a: %d,b: %d, c: %d, d: %d",a,b,c,d);
+    //    d= a && --b || c;
+    //    printf("a: %d,b: %d, c: %d, d: %d",a,b,c,d); //1111
+    // d = --a && --b && c;
+    // printf("a: %d,b: %d, c: %d, d: %d", a, b, c, d); //0210
+    // c = --a || --b ;
+    // printf("a: %d,b: %d, c: %d",a,b,c); //010
+    // c= a || --b;
+    // printf("a: %d,b: %d, c: %d",a,b,c);
+    // c = --a || b;
+    // printf("a: %d,b: %d, c: %d", a, b, c);
+    // c = a && b;
+    // printf("a: %d,b: %d, c: %d",a,b,c);
+    c = --a && b;
+    printf("a: %d,b: %d, c: %d",a,b,c);
 }

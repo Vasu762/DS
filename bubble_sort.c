2e325535b9d149911f9cdf608506f2581fcdcf04
vasu762 <vasuvaghasiya10@gmail.com>
2023-08-22 05:32:15 -0700

    ds

---
+++ bubble_sort.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#define n 8
+int arr[n]= {44,8,15,28,12,35,10,28},a,b;
+void sort()
+{
+    for(int i=0;i<=n;i++)
+    {
+        for(int j=0;j<n-i;j++)
+        {
+            arr[j]>arr[j+1];
+            swapp(a,b);
+            printf("%d",swapp);
+        }
+    }
+}
+int main()
+{
+   int z;
+    z = sort();
+    
+}

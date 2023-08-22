cf1ce014331cdc2b65d0ec85bd98f4447e1eb632
vasu762 <vasuvaghasiya10@gmail.com>
2023-08-22 06:40:53 -0700

    ds

--- bubble_sort.c
+++ bubble_sort.c
@@ -1,27 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
 #define n 8
-int arr[n] = {44, 8, 15, 28, 12, 35, 10, 28};
-void sort()
+int arr[];
+void sort(int arr[])
 {
     for (int i = 0; i <= n; i++)
     {
         for (int j = 0; j < n - i; j++)
         {
-           if( arr[j] > arr[j + 1]){
-            int temp ;
-            temp = arr[j];
-            arr[j]=arr[j+1];
-            arr[j+1]=temp ;
-           }
+            if (arr[j] > arr[j + 1])
+            {
+                int temp;
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
         }
     }
-    for(int i=0;i<=n;i++)
+    for (int i = 0; i <= n; i++)
     {
-    printf("%d   \t",arr[i]);
+        printf("%d   \t", arr[i]);
     }
-    
 }
 int main()
 {
-    sort();
+
+    int arr[n];
+    for (int i = 0; i <= n; i++)
+    {
+        arr[i] = rand() % 50;
+        printf("%d", arr[i]);
+    }
+    sort(arr);
 }
